9be9f7a87e707ddc3fe47af8d2da4a46b39be376
khaleid10 <khaledaloraini@outlook.com>
2026-08-29 20:33:23 +0300

    Fix async generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Yield 10 random numbers asynchronously."""
+    """Yield random numbers between 0 and 10."""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
