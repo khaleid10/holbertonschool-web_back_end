57980eaa89ae237743db06f0c0a79cafe3ca3385
khaleid10 <khaledaloraini@outlook.com>
2026-08-29 21:52:37 +0300

    Fix measure runtime file length

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python3
 """Measure runtime of async comprehensions."""
-
 import asyncio
 import time
 
-async_comprehension = __import__(
-    '1-async_comprehension'
-).async_comprehension
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Measure total runtime for four async comprehensions."""
+    """Measure runtime."""
     start = time.time()
-
-    await asyncio.gather(
-        *(async_comprehension() for _ in range(4))
-    )
-
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
     return time.time() - start
