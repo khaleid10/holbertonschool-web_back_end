bbd55781d075671b725634a9531a7a2375bb5f16
khaleid10 <khaledaloraini@outlook.com>
2026-08-29 21:49:10 +0300

    Fix measure runtime implementation

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Measure the runtime of parallel async comprehensions."""
+"""Measure the runtime of async comprehensions."""
 
 import asyncio
 import time
@@ -10,8 +10,8 @@ async_comprehension = __import__(
 
 
 async def measure_runtime() -> float:
-    """Measure the runtime of four parallel async comprehensions."""
-    start = time.time()
+    """Return the runtime of four parallel async comprehensions."""
+    start = time.perf_counter()
 
     await asyncio.gather(
         async_comprehension(),
@@ -20,5 +20,4 @@ async def measure_runtime() -> float:
         async_comprehension()
     )
 
-    end = time.time()
-    return end - start
+    return time.perf_counter() - start
