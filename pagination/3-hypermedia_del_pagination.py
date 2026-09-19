300e2b5ebb5a96cbd3edc5093a04ba807d34dc00
khaleid10 <khaledaloraini@outlook.com>
2026-09-19 23:27:59 +0300

    Fix pagination default parameters

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -9,18 +9,17 @@ from typing import Dict, List
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
 
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initialize the server."""
         self.__dataset = None
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Return the cached dataset."""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -30,8 +29,7 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0
-        """
+        """Return the dataset indexed by sorting position."""
         if self.__indexed_dataset is None:
             dataset = self.dataset()
             self.__indexed_dataset = {
@@ -40,8 +38,7 @@ class Server:
 
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None,
-                        page_size: int = 10) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """Return deletion-resilient pagination information."""
         if index is None:
             index = 0
@@ -56,15 +53,17 @@ class Server:
         data = []
         current_index = index
 
-        while (len(data) < page_size and
-               current_index < len(self.dataset())):
+        while (
+            len(data) < page_size
+            and current_index < len(self.dataset())
+        ):
             if current_index in indexed_data:
                 data.append(indexed_data[current_index])
             current_index += 1
 
         return {
-            "index": index,
-            "data": data,
-            "page_size": len(data),
-            "next_index": current_index
+            'index': index,
+            'data': data,
+            'page_size': len(data),
+            'next_index': current_index,
         }
